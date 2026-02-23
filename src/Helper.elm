eb18048120fe41cfc7d7e380ea87fcd186f36424
Pris Araiza <prisicilaaraiza@gmail.com>
2026-02-22 18:52:11 -0600

    Corrección de formato

--- src/Helper.elm
+++ src/Helper.elm
@@ -4,17 +4,21 @@ import Char
 import Html exposing (Html, a, div, h1, h2, h3, h4, h5, h6, text)
 import Html.Attributes exposing (href)
 
+
 joinWords : String -> String -> String
 joinWords word1 word2 =
     word1 ++ word2
+
 
 isUpperChars : List Char -> List Bool
 isUpperChars list =
     List.map Char.isUpper list
 
+
 evalChars : List Char -> (Char -> Bool) -> List Bool
 evalChars list funTrans =
     List.map funTrans list
+
 
 headers : String -> Html msg
 headers param =
@@ -26,6 +30,7 @@ headers param =
         , h5 [] [ text param ]
         , h6 [] [ text param ]
         ]
+
 
 hyperlink : String -> String -> Html msg
 hyperlink url linkText =
